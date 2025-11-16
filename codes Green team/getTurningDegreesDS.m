506a2cb77ae13f989a7a67f9eb6ec3e2ab84fa0b
AmbraCassani <ambracassa@gmail.com>
2025-11-16 19:49:11 +0100

    Delete codes Green team/getTurningDegreesDS.m
    Delete file

--- codes Green team/getTurningDegreesDS.m
+++
@@ -1,8 +0,0 @@
-function [ degrees ] = getTurningDegreesDS( gyroV, FS )
-% calculate turning degrees (in abs value)
-%   GyroV is the angular velocity around the vertical axis ALREADY FILTERED in  degrees/second
-winSamples=size(gyroV,1);
-seconds=(1:winSamples)/FS;
-degrees=abs(trapz(seconds,gyroV));
-end
-
